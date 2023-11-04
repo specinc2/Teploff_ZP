c3c7bd85fe7c809b8173396944d88879abf738e3
Spec <specinc@yandex.ru>
2023-11-04 18:34:37 +0300

    the end

--- app/src/main/java/com/example/teploff_zp/MainActivity.kt
+++ app/src/main/java/com/example/teploff_zp/MainActivity.kt
@@ -4,46 +4,64 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.text.toInt
 import kotlin.text.toInt as kotlinTextToInt
 
 
 class MainActivity : AppCompatActivity() {
-var counter : Boolean = false;
+    var counter : Boolean = false;
+    internal lateinit var btnSwitch: Switch;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var editTextNumber : EditText? = findViewById<EditText>(R.id.editTextNumber);
         var editTextNumber2 : EditText? = findViewById<EditText>(R.id.editTextNumber2);
-        var editTextNumber3 : EditText? = findViewById<EditText>(R.id.sumozon);
         var button : Button = findViewById<Button>(R.id.button);
+
+        var editTextNumber3  = findViewById<EditText>(R.id.sumozon);
+        var ozon2 : Int = 0;
+        btnSwitch = findViewById<View>(R.id.switch2) as Switch;
+        btnSwitch.setOnClickListener {
+            if (btnSwitch.isChecked){
+                editTextNumber3.visibility = View.VISIBLE
+                }
+            else {
+                editTextNumber3.visibility = View.INVISIBLE
+
+                }
+        }
         //var switch = findViewById<Switch>(R.id.switch2);
         //switch.setOnCheckedChangeListener(View.VISIBLE);
-
-
-
     }
+
+
 
     fun onClick(view: View) {
         var switch = findViewById<Switch>(R.id.switch2);
         var editTextNumber3  = findViewById<EditText>(R.id.sumozon);
-        if (switch.isChecked)
-            editTextNumber3.visibility = if (editTextNumber3.visibility == View.INVISIBLE){
-                View.VISIBLE
-            }
-            else {
-                View.INVISIBLE
-            }
         val editTextNumber  = findViewById<EditText>(R.id.editTextNumber);
         val editTextNumber2  = findViewById<EditText>(R.id.editTextNumber2);
-
+        var ozon : Int;
+        var ozon2 : Int;
+        var dini : Int;
+        var dini2 : Int;
+        val itog : Int;
         val textView4 = findViewById<TextView>(R.id.textView4);
         var itog2 : Int = editTextNumber2.text.toString().kotlinTextToInt();
-        var ozon : Int = editTextNumber3.text.toString().kotlinTextToInt();
-        var ozon2 : Int = ozon / 100;
-        var dini : Int = editTextNumber.text.toString().kotlinTextToInt();
-        var dini2 : Int = dini * 700;
-        val itog : Int = dini2 + (itog2 / 100 * 3) + ozon2 - 2600;
+        if (btnSwitch.isChecked){
+            ozon = editTextNumber3.text.toString().kotlinTextToInt();
+            ozon2 = ozon / 100;
+            dini = editTextNumber.text.toString().kotlinTextToInt();
+            dini2 = dini * 700;
+            itog = dini2 + (itog2 / 100 * 3) + ozon2 - 2600;
+        }
+        else
+        {
+            dini = editTextNumber.text.toString().kotlinTextToInt();
+            dini2 = dini * 700;
+            itog = dini2 + (itog2 / 100 * 3) - 2600;
+        }
         textView4.text = "Ты заработал немножко денежек!: " + itog + " рубликов";
     }
 
